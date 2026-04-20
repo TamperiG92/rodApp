a10bbfd71274f0df182dc0a0002e53b57b718be3
newolad <newolad.development@gmail.com>
2026-04-19 23:14:56 -0500

    carga prueba

---
+++ app/src/main/java/com/example/rodapp/activities/main/MantenimientoAlertasActivity.kt
@@ -0,0 +1,14 @@
+package com.example.rodapp.activities.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.rodapp.R
+
+class MantenimientoAlertasActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mantenimiento_alertas)
+        
+        // Aquí puedes inicializar tus vistas y lógica
+    }
+}
